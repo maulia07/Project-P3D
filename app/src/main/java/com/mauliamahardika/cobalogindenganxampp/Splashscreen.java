91b105f15cef9a446cc1778cf5a270ebef4e165c
maulia <mauliamahardika99@gmail.com>
2021-09-14 09:59:30 +0700

    V1 14 SEPTEMBER 2021

---
+++ app/src/main/java/com/mauliamahardika/cobalogindenganxampp/Splashscreen.java
@@ -0,0 +1,27 @@
+package com.mauliamahardika.cobalogindenganxampp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },3000);
+    }
+}
